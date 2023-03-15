0809ce59abe657bb91c8eeb2ddf85c56b5e24ace
pranjalpandey693 <pranjalpandey693@gmail.com>
2023-03-15 19:02:44 +0530

    new

--- sortingalgo/quicksort.cpp
+++ sortingalgo/quicksort.cpp
@@ -1,11 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int part(int arr[], int low ,int high)
+{       
+    int pivot = arr[low];
+    int i=low;
+    int j=high;
+    while (i<j)
+    {
+       while(arr[i]<=pivot&& i<=high-1)
+       {
+        i++;
+       }
+       while(arr[j]>pivot && j>=low+1)
+       {
+        j--;
+       }
+       if(i<j)
+       {
+        swap(arr[i],arr[j]);
+
+       }
+       swap(arr[low],arr[j]);
+       return j;
+    }
+    
+   
+}
+
+void qs(int arr[],int low, int high)
+{      if(low<high){
+    int pindex = part(arr,low,high);
+    qs(arr,low,pindex-1);
+    qs(arr,pindex+1,high);
+}
+
+}
 
 void print(int arr[] ,int n){
+        cout << "the modified array is \n";
     for(int i=0;i<n;i++)
-    {
-        cout << arr[i] << endl;
+    {    cout << arr[i] << " ";
 
     }
 }
@@ -19,5 +54,6 @@ int main(){
     {
         cin >> arr[i];
     }
-    print(arr, arr.size());
+   qs(arr,0,n-1);
+   print (arr,n);
 }
